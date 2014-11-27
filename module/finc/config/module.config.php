89fe804c4c6c49e8c624b4a22265a0fe1eed5e40
Martin Czygan <martin.czygan@gmail.com>
2014-11-27 14:34:35 +0100

    Implementation of fullrecord retrieval from external source as a custom module (finc).
    Cherry-picked from: d74a2bfba5f1bc05bc57376fd92fbd9cf316a135

---
+++ module/finc/config/module.config.php
@@ -0,0 +1,29 @@
+<?php
+namespace finc\Module\Configuration;
+
+$config = array(
+    'vufind' => array(
+        'plugin_managers' => array(
+            'recorddriver' => array(
+                'factories' => array(
+                    'solrmarclite' => 'finc\RecordDriver\Factory::getSolrMarcLite'
+                ),
+            ),
+        ),
+        'recorddriver_tabs' => array(
+            'finc\RecordDriver\SolrMarcLite' => array(
+                'tabs' => array(
+                    'Holdings' => 'HoldingsILS', 'Description' => 'Description',
+                    'TOC' => 'TOC', 'UserComments' => 'UserComments',
+                    'Reviews' => 'Reviews', 'Excerpt' => 'Excerpt',
+                    'Preview' => 'preview',
+                    'HierarchyTree' => 'HierarchyTree', 'Map' => 'Map',
+                    'Details' => 'StaffViewMARC',
+                ),
+                'defaultTab' => null,
+            ),
+        ),
+    ),
+);
+
+return $config;
