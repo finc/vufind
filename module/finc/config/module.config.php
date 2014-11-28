e1b6c842b5e4a2bbb6b000cd3cb6c21240da19f3
André Lahmann <lahmann@ub.uni-leipzig.de>
2014-11-28 15:33:48 +0100

    refs #4283: * added latest DAIA-Driver from github.com/vufind-org to finc-module * added latest PAIA-Driver from github.com/gbv/vufind VZG-module to finc-module * updated finc-module configuration with DAIA/PAIA drivers

--- module/finc/config/module.config.php
+++ module/finc/config/module.config.php
@@ -4,6 +4,12 @@ namespace finc\Module\Configuration;
 $config = array(
     'vufind' => array(
         'plugin_managers' => array(
+            'ils_driver' => array(
+                'invokables' => array(
+                    'daia' => 'finc\ILS\Driver\DAIA',
+                    'paia' => 'finc\ILS\Driver\PAIA',
+                ),
+            ),
             'recorddriver' => array(
                 'factories' => array(
                     'solrmarcremote' => 'finc\RecordDriver\Factory::getSolrMarcRemote'
