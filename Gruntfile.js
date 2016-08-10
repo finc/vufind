ec3b72e516056c6362d293d41803fd0b67e7f990
André Lahmann <lahmann@ub.uni-leipzig.de>
2016-08-10 11:26:54 +0200

    suggestion: * added sass_recursive task for searching themes for scss files recursively

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,6 +57,23 @@ module.exports = function(grunt) {
         }
       }
     },
+    sass_recursive: {
+      sass: {
+        dist: {
+          options: {
+            outputStyle: 'compressed' // specify style here
+          }
+        },
+        dev: {
+          options: {
+            outputStyle: 'expanded'
+          }
+        },
+        options: {
+          themeFolder: 'themes'
+        }
+      }
+    },
     // Convert LESS to SASS, mostly for development team use
     lessToSass: {
       convert: {
@@ -124,5 +141,42 @@ module.exports = function(grunt) {
         }
       }
     }
+  });
+  grunt.registerMultiTask('sass_recursive', function (arg1, arg2) {
+    var fs = require('fs')
+        , path = require('path')
+        , options = (arguments.length > 0 && this.data[arg1] && this.data[arg1].options) ? this.data[arg1].options : this.data.dist.options
+        , theme = (arguments.length > 1) ? arg2 : null
+        , themeFolder = this.data.options.themeFolder || 'themes'
+        , themeList = fs.readdirSync(path.resolve(themeFolder))
+        , sassConfig = {}
+        ;
+
+    for (var i in themeList) {
+      if (theme && themeList[i] !== theme) {
+        continue;
+      }
+      var sassDir = path.join(themeFolder, themeList[i], 'scss');
+      var cssDir = path.join(themeFolder, themeList[i], 'css');
+
+      try {
+        fs.statSync(sassDir);
+        sassConfig[themeList[i]] = {
+          options: options,
+          files: [{
+            expand: true,
+            cwd: sassDir,
+            src: ['**/*.scss'],
+            dest: cssDir,
+            ext: '.css'
+          }]
+        };
+      } catch (err) {
+        // silently suppress thrown errors when no sass sources exist in a theme
+      }
+    }
+
+    grunt.config.set('sass', sassConfig);
+    grunt.task.run('sass');
   });
 };
