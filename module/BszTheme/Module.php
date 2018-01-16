c0276dbbc640df367e78cc992f045971683e1679
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2018-01-16 10:29:57 +0100

    Added empty BszTheme module. This must be added in httpd-vufind.conf

---
+++ module/BszTheme/Module.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace BszTheme;
+use Zend\Mvc\MvcEvent;
+/**
+ * Bsz theme adaption
+ *
+ * @author Cornelius Amzar <cornelius.amzar@bsz-bw.de>
+ */
+class Module extends \VuFindTheme\Module
+{
+
+}
