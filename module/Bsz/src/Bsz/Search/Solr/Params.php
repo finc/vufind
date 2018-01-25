b54836ef85cf21af63a4bc9284cb05c185f8da82
winkler <stefan.winkler@bsz-bw.de>
2018-01-25 13:45:26 +0100

    enable boosting (for SWB and Date)

--- module/Bsz/src/Bsz/Search/Solr/Params.php
+++ module/Bsz/src/Bsz/Search/Solr/Params.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Bsz\Search\Solr;
+use VuFindSearch\ParamBag;
 
 /**
  * Description of Params
@@ -54,4 +55,83 @@ class Params extends \VuFind\Search\Solr\Params
         }
         return $filterQuery;
     }
+    
+    /**
+     * Create search backend parameters for advanced features.
+     *
+     * @return ParamBag
+     */
+    public function getBackendParameters()
+    {
+        $backendParams = new ParamBag();
+        $backendParams->add('year', (int)date('Y')+1);            
+
+        // Spellcheck
+        $backendParams->set(
+            'spellcheck', $this->getOptions()->spellcheckEnabled() ? 'true' : 'false'
+        );
+
+        // Facets
+        $facets = $this->getFacetSettings();
+        if (!empty($facets)) {
+            $backendParams->add('facet', 'true');
+
+            foreach ($facets as $key => $value) {
+                // prefix keys with "facet" unless they already have a "f." prefix:
+                $fullKey = substr($key, 0, 2) == 'f.' ? $key : "facet.$key";
+                $backendParams->add($fullKey, $value);
+            }
+            $backendParams->add('facet.mincount', 1);
+        }
+
+        // Filters
+        $filters = $this->getFilterSettings();
+        foreach ($filters as $filter) {
+            $backendParams->add('fq', $filter);
+        }
+
+        // Shards
+        $allShards = $this->getOptions()->getShards();
+        $shards = $this->getSelectedShards();
+        if (empty($shards)) {
+            $shards = array_keys($allShards);
+        }
+
+        // If we have selected shards, we need to format them:
+        if (!empty($shards)) {
+            $selectedShards = [];
+            foreach ($shards as $current) {
+                $selectedShards[$current] = $allShards[$current];
+            }
+            $shards = $selectedShards;
+            $backendParams->add('shards', implode(',', $selectedShards));
+        }
+
+        // Sort
+        $sort = $this->getSort();
+        if ($sort) {
+            // If we have an empty search with relevance sort, see if there is
+            // an override configured:
+            if ($sort == 'relevance' && $this->getQuery()->getAllTerms() == ''
+                && ($relOv = $this->getOptions()->getEmptySearchRelevanceOverride())
+            ) {
+                $sort = $relOv;
+            }
+            $backendParams->add('sort', $this->normalizeSort($sort));
+        }
+
+        // Highlighting -- on by default, but we should disable if necessary:
+        if (!$this->getOptions()->highlightEnabled()) {
+            $backendParams->add('hl', 'false');
+        }
+
+        // Pivot facets for visual results
+
+        if ($pf = $this->getPivotFacets()) {
+            $backendParams->add('facet.pivot', $pf);
+        }
+
+        return $backendParams;
+    }
+    
 }
