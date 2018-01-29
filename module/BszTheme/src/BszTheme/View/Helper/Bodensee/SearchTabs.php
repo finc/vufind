e6f7220a10bd6ba907b1ec8dc24a55e237e05efe
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2018-01-29 10:27:39 +0100

    SearchTabs moved

---
+++ module/BszTheme/src/BszTheme/View/Helper/Bodensee/SearchTabs.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BszTheme\View\Helper\Bodensee;
+
+use VuFind\Search\Base\Results;
+use VuFind\Search\Results\PluginManager;
+use VuFind\Search\SearchTabsHelper;
+use Zend\Http\Request;
+use Zend\View\Helper\Url;
+
+/**
+ * BSZ extension of searchTabs View Helper
+ *
+ * @author Cornelius Amzar <cornelius.amzar@bsz-bw.de>
+ */
+class SearchTabs extends \VuFind\View\Helper\Root\SearchTabs
+{
+    //put your code here
+}
