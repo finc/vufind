a81f4d635c693f7ed525cdab4db0f8c723181164
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2018-02-09 15:34:58 +0100

    FAvicon works

--- module/BszTheme/src/BszTheme/ThemeInfo.php
+++ module/BszTheme/src/BszTheme/ThemeInfo.php
@@ -97,7 +97,12 @@ class ThemeInfo extends \VuFindTheme\ThemeInfo {
      * @return string
      */
     public function addClientFavicon() {
-        return 'favicon/'.$this->tag.'.ico';
+        if (file_exists($this->baseDir.'/'.$this->currentTheme.'/images/favicon/'.$this->tag.'.ico')) {
+            return 'favicon/'.$this->getTag().static::FAVICON_TYPE;            
+        } else {
+            return 'favicon/default.ico';                       
+        }
+
     }
 }
 
