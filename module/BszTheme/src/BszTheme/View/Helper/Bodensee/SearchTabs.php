21562ea58358b9ef7edb06dd9c16ca15ae43dd35
stefan.winkler@bsz-bw.de <stefan.winkler@bsz-bw.de>
2018-02-16 14:10:22 +0100

    SearchTab Icons added

--- module/BszTheme/src/BszTheme/View/Helper/Bodensee/SearchTabs.php
+++ module/BszTheme/src/BszTheme/View/Helper/Bodensee/SearchTabs.php
@@ -22,4 +22,136 @@ class SearchTabs extends \VuFind\View\Helper\Root\SearchTabs
         }
         return false;
     }
+    
+    /**
+     * Create information representing a selected tab.
+     *
+     * @param string $id             Tab ID
+     * @param string $class          Search class ID
+     * @param string $label          Display text for tab
+     * @param string $permissionName Name of a permissionrule
+     *
+     * @return array
+     */
+    protected function createSelectedTab($id, $class, $label, $permissionName)
+    {
+        return [
+            'id' => $id,
+            'class' => $class,
+            'icon' => $this->getIcon($class),            
+            'label' => $label,
+            'permission' => $permissionName,
+            'selected' => true
+        ];
+    }
+
+    /**
+     * Create information representing a basic search tab.
+     *
+     * @param string $id             Tab ID
+     * @param string $class          Search class ID
+     * @param string $label          Display text for tab
+     * @param string $newUrl         Target search URL
+     * @param string $permissionName Name of a permissionrule
+     *
+     * @return array
+     */
+    protected function createBasicTab($id, $class, $label, $newUrl, $permissionName)
+    {
+        return [
+            'id' => $id,
+            'class' => $class,
+            'icon' => $this->getIcon($class),            
+            'label' => $label,
+            'permission' => $permissionName,
+            'selected' => false,
+            'url' => $newUrl
+        ];
+    }
+
+    /**
+     * Create information representing a tab linking to "search home."
+     *
+     * @param string $id             Tab ID
+     * @param string $class          Search class ID
+     * @param string $label          Display text for tab
+     * @param array  $filters        Tab filters
+     * @param string $permissionName Name of a permissionrule
+     *
+     * @return array
+     */
+    protected function createHomeTab($id, $class, $label, $filters, $permissionName)
+    {
+        // If an advanced search is available, link there; otherwise, just go
+        // to the search home:
+        $results = $this->results->get($class);
+        $url = $this->url->__invoke($results->getOptions()->getSearchHomeAction())
+            . $this->buildUrlHiddenFilters($results, $filters);
+        return [
+            'id' => $id,
+            'class' => $class,
+            'icon' => $this->getIcon($class),            
+            'label' => $label,
+            'permission' => $permissionName,
+            'selected' => false,
+            'url' => $url
+        ];
+    }
+
+    /**
+     * Create information representing an advanced search tab.
+     *
+     * @param string $id             Tab ID
+     * @param string $class          Search class ID
+     * @param string $label          Display text for tab
+     * @param array  $filters        Tab filters
+     * @param string $permissionName Name of a permissionrule
+     *
+     * @return array
+     */
+    protected function createAdvancedTab($id, $class, $label, $filters,
+        $permissionName
+    ) {
+        // If an advanced search is available, link there; otherwise, just go
+        // to the search home:
+        $results = $this->results->get($class);
+        $options = $results->getOptions();
+        $advSearch = $options->getAdvancedSearchAction();
+        $url = $this->url
+            ->__invoke($advSearch ? $advSearch : $options->getSearchHomeAction())
+            . $this->buildUrlHiddenFilters($results, $filters);
+        return [
+            'id' => $id,
+            'class' => $class,
+            'icon' => $this->getIcon($class),            
+            'label' => $label,
+            'permission' => $permissionName,
+            'selected' => false,
+            'url' => $url
+        ];
+    }
+    
+    /**
+     * Get Font Awesome Icon for tab
+     * @param string $class
+     * @return string
+     */
+    public static function getIcon($class) {
+
+        switch (strtolower($class)) {
+            case 'eds': $icon = 'fa-newspaper-o';
+                break;
+            case 'summon': $icon = 'fa-newspaper-o';
+                break;
+            case 'solr': $icon = 'fa-book';
+                break;
+            case 'interlending': $icon = 'fa-globe';
+                break;    
+            case 'fis': $icon = 'fa-university';
+                break;    
+            default: $icon = 'fa-question-circle';
+                break;
+        }
+        return $icon;
+    }    
 }
