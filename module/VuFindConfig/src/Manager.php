ae17c04802ab9575a288942926cce6a70587ef90
Sebastian Kehr <kehr@ub.uni-leipzig.de>
2018-03-05 20:35:09 +0100

    refs #12600; added GPL license headers and copyright notices

--- module/VuFindConfig/src/Manager.php
+++ module/VuFindConfig/src/Manager.php
@@ -1,5 +1,30 @@
 <?php
-
+/**
+ * VuFind Config Manager
+ *
+ * Copyright (C) 2010 Villanova University,
+ *               2018 Leipzig University <info.ub.uni-leipzig.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc. 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @category VuFind
+ * @package  VuFindConfig
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Sebastian Kehr <kehr@ub.uni-leipzig.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU GPLv2
+ * @link     https://vufind.org/wiki/development Wiki
+ */
 namespace VuFind\Config;
 
 use Symfony\Component\Yaml\Yaml as YamlParser;
