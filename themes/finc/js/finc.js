65eb3882ebf2712ec409938459348674023c6f68
Claas Kazzer <kazzer@ub.uni-leipzig.de>
2018-07-11 12:51:44 +0200

    refs #13625 * adds fix for padding-right-jump on modal-open

--- themes/finc/js/finc.js
+++ themes/finc/js/finc.js
@@ -1,10 +1,9 @@
 // remove when fixed in BS
 
-// Remove/add aria-hidden + add modalTitle for screen-reader access
+// Remove/add aria-hidden + add modalTitle for screen-reader access, check of .focus() is necessary
 $(document).on('shown.bs.modal', function () {
-  $('#modal').attr('aria-hidden', 'false').show();
+  $('#modal').attr('aria-hidden', 'false').show().focus();
   $('#modal h2').attr('id', 'modalTitle');
-  $('#modal').focus(); // check if necessary!
 });
 
 $(document).on('hidden.bs.modal', function () {
