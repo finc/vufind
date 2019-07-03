f73cda56651008bb60a3bf2aef39bbf047bae6c4
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2019-07-03 15:22:24 +0200

    Catch some cases where getAuth might be called if no library available.

--- module/Bsz/src/Bsz/Controller/RecordController.php
+++ module/Bsz/src/Bsz/Controller/RecordController.php
@@ -45,7 +45,7 @@ class RecordController extends \VuFind\Controller\RecordController
     use \VuFind\Controller\StorageRetrievalRequestsTrait;
     use \VuFind\Log\LoggerAwareTrait;
     
-    const TIMEOUT = 30;
+    const TIMEOUT = 120;
     
     protected $orderId = 0;
     
