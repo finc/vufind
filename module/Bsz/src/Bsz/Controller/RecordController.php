fdfe4a8677ab43ee52be410d38a9c2b4241c0611
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2019-07-03 14:26:40 +0200

    Catch some cases where getAuth might be called if no library available.

--- module/Bsz/src/Bsz/Controller/RecordController.php
+++ module/Bsz/src/Bsz/Controller/RecordController.php
@@ -131,7 +131,7 @@ class RecordController extends \VuFind\Controller\RecordController
         $first = $libraries->getFirstActive($client->getIsils());
         $submitDisabled = false;
         
-        if ($authManager->loginEnabled() 
+        if (isset($first) && $authManager->loginEnabled() 
                 && !$authManager->isLoggedIn()
                 && $first->getAuth() == 'shibboleth') {
             $this->FlashMessenger()->addErrorMessage('You must be logged in first');
