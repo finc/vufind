739fcce46bdb627877285aab6d7413263d49e4a7
Stefan Winkler <stefan.winkler@bsz-bw.de>
2019-07-10 11:54:10 +0200

    dedup commented out in resultsAction()

--- module/Bsz/src/Bsz/Controller/SearchController.php
+++ module/Bsz/src/Bsz/Controller/SearchController.php
@@ -28,13 +28,13 @@ class SearchController extends \VuFind\Controller\SearchController
     
     public function resultsAction()
     {
-        $dedup = $this->getServiceLocator()->get('Bsz/Config/Dedup');
+        //$dedup = $this->getServiceLocator()->get('Bsz/Config/Dedup');
         $isils = $this->params()->fromQuery('isil');
         if (count($isils) > 0) {
             return $this->processIsil();
         }
         $view = Parent::resultsAction();
-        $view->dedup = $dedup->isActive();
+        //$view->dedup = $dedup->isActive();
         return $view;
     }    
 }
