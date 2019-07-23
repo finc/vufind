9628781c0acff358245dcf3976ef7574cb695bb0
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2019-07-23 14:22:48 +0200

    Aliases in theme.config.php

--- themes/bodensee/theme.config.php
+++ themes/bodensee/theme.config.php
@@ -8,14 +8,23 @@ $config = [
     ],     
     'helpers' => [
         'factories' => [
-            'VuFind\View\Helper\Bootstrap3\LayoutClass' => 'BszTheme\View\Helper\Bodensee\Factory::getLayoutClass',
-            'VuFind\View\Helper\Root\OpenUrl' => 'BszTheme\View\Helper\Bodensee\Factory::getOpenUrl',
-            'VuFind\View\Helper\Root\Record' => 'BszTheme\View\Helper\Bodensee\Factory::getRecord',
-            'VuFind\View\Helper\Root\RecordLink' => 'BszTheme\View\Helper\Bodensee\Factory::getRecordLink',
-            'VuFind\View\Helper\Root\Piwik' => 'BszTheme\View\Helper\Bodensee\Factory::getPiwik',
-            'VuFind\View\Helper\Root\SearchTabs' => 'BszTheme\View\Helper\Bodensee\Factory::getSearchTabs',
-            'VuFind\View\Helper\Root\SearchMemory' => 'BszTheme\View\Helper\Bodensee\Factory::getSearchMemory',
+            'Bsz\View\Helper\Bootstrap3\LayoutClass' => 'BszTheme\View\Helper\Bodensee\Factory::getLayoutClass',
+            'Bsz\View\Helper\Root\OpenUrl' => 'BszTheme\View\Helper\Bodensee\Factory::getOpenUrl',
+            'Bsz\View\Helper\Root\Record' => 'BszTheme\View\Helper\Bodensee\Factory::getRecord',
+            'Bsz\View\Helper\Root\RecordLink' => 'BszTheme\View\Helper\Bodensee\Factory::getRecordLink',
+            'Bsz\View\Helper\Root\Piwik' => 'BszTheme\View\Helper\Bodensee\Factory::getPiwik',
+            'Bsz\View\Helper\Root\SearchTabs' => 'BszTheme\View\Helper\Bodensee\Factory::getSearchTabs',
+            'Bsz\View\Helper\Root\SearchMemory' => 'BszTheme\View\Helper\Bodensee\Factory::getSearchMemory',
         ],
+        'aliases' => [
+            'VuFind\View\Helper\Bootstrap3\LayoutClass' => 'Bsz\View\Helper\Bootstrap3\LayoutClass',
+            'VuFind\View\Helper\Root\OpenUrl' => 'Bsz\View\Helper\Root\OpenUrl',
+            'VuFind\View\Helper\Root\Record' => 'Bsz\View\Helper\Root\Record',
+            'VuFind\View\Helper\Root\RecordLink' => 'Bsz\View\Helper\Root\RecordLink',
+            'VuFind\View\Helper\Root\Piwik' => 'Bsz\View\Helper\Root\Piwik',
+            'VuFind\View\Helper\Root\SearchTabs' => 'Bsz\View\Helper\Root\SearchTabs',
+            'VuFind\View\Helper\Root\SearchMemory' => 'Bsz\View\Helper\Root\SearchMemory'
+        ]
     ]
 ];
 return $config;
