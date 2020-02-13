eb69b720b7fb60ec9ac9d7334277d29833d8d71f
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2020-02-13 13:31:43 +0100

    Fixed unit tests

--- module/VuFind/tests/unit-tests/src/VuFindTest/UrlShortener/DatabaseTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/UrlShortener/DatabaseTest.php
@@ -72,17 +72,20 @@ class DatabaseTest extends TestCase
             ->getMock();
     }
 
-    /**
-     * Test that the shortener works correctly under "happy path."
-     *
-     * @return void
-     * @throws Exception
-     */
     public function testShortener()
     {
         $table = $this->getMockTable(['insert', 'select']);
         $table->expects($this->once())->method('insert')
             ->with($this->equalTo(['path' => '/bar', 'hash' => 'a1e7812e2']));
+        $mockResults = $this->getMockBuilder(ResultSet::class)
+            ->setMethods(['count', 'current'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $mockResults->expects($this->once())->method('count')
+            ->will($this->returnValue(0));
+        $table->expects($this->once())->method('select')
+            ->with($this->equalTo(['hash' => 'a1e7812e2']))
+            ->will($this->returnValue($mockResults));
         $db = $this->getShortener($table);
         $this->assertEquals('http://foo/short/a1e7812e2', $db->shorten('http://foo/bar'));
     }
