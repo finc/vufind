91b5a70cb44a7a213fbde168be3bb2da8eceafbb
Stefan Winkler <stefan.winkler@bsz-bw.de>
2020-03-02 16:24:20 +0100

    see searchterms from simple search ($lookfor) in detail view

--- module/BszTheme/src/BszTheme/View/Helper/Bodensee/SearchMemory.php
+++ module/BszTheme/src/BszTheme/View/Helper/Bodensee/SearchMemory.php
@@ -14,18 +14,30 @@ namespace BszTheme\View\Helper\Bodensee;
  * @author amzar
  */
 class SearchMemory extends \VuFind\View\Helper\Root\SearchMemory {
-    
+
     /**
-     * Use this instead of getLastSearchLink if you don't want any markup. 
-     * 
+     * Use this instead of getLastSearchLink if you don't want any markup.
+     *
      * @return string
      */
     public function getLastSearchUrl() {
         $last = $this->memory->retrieveSearch();
         if (!empty($last)) {
             $escaper = $this->getView()->plugin('escapeHtml');
-            return $escaper($last);                 
+            return $escaper($last);
         }
         return '';
     }
+    /**
+     *  get searchterms from session
+     *
+     * @return string
+     */
+    public function getLastSearchterms() {
+        $url = $this->memory->retrieveSearch();
+        $query_str = parse_url($url,PHP_URL_QUERY);
+        parse_str($query_str, $queryArray);
+        return isset($queryArray['lookfor']) ? $queryArray['lookfor'] : null;
+    }
+
 }
