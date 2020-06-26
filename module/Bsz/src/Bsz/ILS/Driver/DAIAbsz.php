1f415a05d66a5c3b8e7002199fdd3ae76426a7ef
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2020-06-26 09:27:10 +0200

    DLR: Why is isil required here? ISIL is set in the DAIA URL.

--- module/Bsz/src/Bsz/ILS/Driver/DAIAbsz.php
+++ module/Bsz/src/Bsz/ILS/Driver/DAIAbsz.php
@@ -439,10 +439,6 @@ class DAIAbsz extends \VuFind\ILS\Driver\DAIA
      */
     public function hasHoldings($id)
     {
-        // we can't query DAIA without an ISIL.
-        if (empty($this->isil)) {
-            return false;
-        }
         $holdings = $this->getHolding($id);
 
         if (isset($holdings) && count($holdings) > 0) {
