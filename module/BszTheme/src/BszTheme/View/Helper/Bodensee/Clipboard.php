fcebc5ca228b44880b3f70457c14e796e512d631
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2020-10-02 14:59:02 +0200

    Clipboard biew helper.
    This could be a basic exmple of how to create a custom view helper.

---
+++ module/BszTheme/src/BszTheme/View/Helper/Bodensee/Clipboard.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Copyright 2020 (C) Bibliotheksservice-Zentrum Baden-
+ * Württemberg, Konstanz, Germany
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+namespace BszTheme\View\Helper\Bodensee;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * Add a copy to clipboard icon to text content
+ * @package  BszTheme\View\Helper\Bodensee
+ * @category boss
+ * @author   Cornelius Amzar <cornelius.amzar@bsz-bw.de>
+ */
+class Clipboard extends AbstractHelper
+{
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    public function __invoke(string $content)
+    {
+        return $this->getView()->render('Helpers/clipboard.phtml', [
+            'content' => $content
+        ]);
+    }
+}
