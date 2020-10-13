553319f0411adae7d1288f7217a35e4758cb9d9a
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2020-10-13 12:15:56 +0200

    Namespace added

--- module/BszTheme/tests/unit-tests/src/View/Helper/ClientAssetTest.php
+++ module/BszTheme/tests/unit-tests/src/View/Helper/ClientAssetTest.php
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+namespace BszTheme\View\Helper;
 
 use BszTheme\View\Helper\ClientAsset;
 use PHPUnit\Framework\TestCase;
