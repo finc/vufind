08cd43bb9768eb99d94992b9aef57ba32cc30124
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2021-07-07 16:02:44 +0200

    Added helper function for latter use in cover loading

--- themes/bodensee/js/additions.js
+++ themes/bodensee/js/additions.js
@@ -486,6 +486,16 @@ function recordCoverAjax() {
         }
     });
 }
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
 
 
 /*
