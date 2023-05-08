74856f2180df95465706cb01650c694d219e9dc0
elsenhans <elsenhans@users.noreply.github.com>
2023-05-08 15:57:28 +0200

    improve accessibility for checkboxes
    improve accessibility for checkboxes in holds/list.phtml
    
    add translations for improve accessibility for checkboxes in account functions
    
    change order of translations
    
    change clipboard to book bag; add missing transEscAttr
    
    change recall to recalls
    
    remove with_selected language token
    
    add new language token select_all_on_page; change entries to items
    
    fix typo to: items on the page
    
    Update en.ini
    
    use select_all_on_page in checkedout.phtml and list.phtml
    
    renaming of view helper method: getTitleDescribedById => getUniqueIdWithSource
    
    use escapeHtmlAttr() for $describedById and $cartId
    
    cart: add id in contents to reference from cart-buttons
    
    uniqueId: use getUniqueIdWithSource() consistently in lists and toolbar
    
    uniqueId: use getUniqueIdWithSource() in record view helper getCheckbox
    
    change translations for select_item_hold_cancel
    
    rename method for id with source and introduce new for templates to get id without spaces
    
    undo changes for cart described by id (conflicts with js?)
    
    use prefix for lightbox
    
    add prefix for cart content
    
    add prefix for cart content
    
    add prefix for cart content - fix multiline
    
    add context as prefix for id
    
    set context as string in templates
    
    move generation of id to record view helper
    
    adapt record test for prefixing
    
    fix code styles
    
    fix code styles

--- module/VuFind/src/VuFind/View/Helper/Root/Record.php
+++ module/VuFind/src/VuFind/View/Helper/Root/Record.php
@@ -29,6 +29,7 @@
 
 namespace VuFind\View\Helper\Root;
 
+use Laminas\View\Helper\ViewModel;
 use VuFind\Cover\Router as CoverRouter;
 
 /**
@@ -57,6 +58,13 @@ class Record extends \Laminas\View\Helper\AbstractHelper
      * @var CoverRouter
      */
     protected $coverRouter = null;
+
+    /**
+     * ViewModel helper
+     *
+     * @var ViewModel
+     */
+    protected $viewModel = null;
 
     /**
      * Record driver
@@ -92,6 +100,18 @@ class Record extends \Laminas\View\Helper\AbstractHelper
     public function setCoverRouter($router)
     {
         $this->coverRouter = $router;
+    }
+
+    /**
+     * Inject the layout helper
+     *
+     * @param ViewModel $viewModel layout view helper
+     *
+     * @return void
+     */
+    public function setViewModelHelper(ViewModel $viewModel)
+    {
+        $this->viewModel = $viewModel;
     }
 
     /**
@@ -415,8 +435,7 @@ class Record extends \Laminas\View\Helper\AbstractHelper
      */
     public function getCheckbox($idPrefix = '', $formAttr = false, $number = null)
     {
-        $id = $this->driver->getSourceIdentifier() . '|'
-            . $this->driver->getUniqueId();
+        $id = $this->getUniqueHtmlElementId($idPrefix);
         $context
             = ['id' => $id, 'number' => $number, 'prefix' => $idPrefix];
         if ($formAttr) {
@@ -706,5 +725,48 @@ class Record extends \Laminas\View\Helper\AbstractHelper
     protected function deduplicateLinks($links)
     {
         return array_values(array_unique($links, SORT_REGULAR));
+    }
+
+    /**
+     * Get the source identifier + unique id of the record without spaces
+     *
+     * @param string $idPrefix Prefix for HTML ids
+     *
+     * @return string
+     */
+    public function getUniqueHtmlElementId($idPrefix = "")
+    {
+        if (empty($idPrefix)) {
+            if ($this->viewModel->getCurrent()->getTemplate()) {
+                $idPrefix .= str_replace(
+                    DIRECTORY_SEPARATOR,
+                    '-',
+                    $this->viewModel->getCurrent()->getTemplate()
+                );
+            }
+        }
+
+        if ($this->viewModel->getRoot()->getTemplate() === 'layout/lightbox') {
+            $idPrefix = 'modal-' . $idPrefix;
+        }
+
+        return preg_replace(
+            "/\s+/",
+            "_",
+            ($idPrefix ? $idPrefix . '-' : '') . $this->getUniqueIdWithSourcePrefix()
+        );
+    }
+
+    /**
+     * Get the source identifier + unique id of the record
+     *
+     * @return string
+     */
+    public function getUniqueIdWithSourcePrefix()
+    {
+        if ($this->driver) {
+            return "{$this->driver->tryMethod('getSourceIdentifier')}"
+                . "|{$this->driver->tryMethod('getUniqueId')}";
+        }
     }
 }
